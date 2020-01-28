a3a33aefc7490e689a53705fac4fc60cf2aeb27a
yd4u2c <yd4u2c@yahoo.com>
2020-01-28 23:09:59 +0100

    individual table

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 
 class RegisterController extends Controller
 {
@@ -74,38 +72,9 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-        $table_name = 'demo';
- 
-        // set your dynamic fields (you can fetch this data from database this is just an example)
-        $fields = [
-            ['name' => 'field_1', 'type' => 'string'],
-            ['name' => 'field_2', 'type' => 'text'],
-            ['name' => 'field_3', 'type' => 'integer'],
-            ['name' => 'field_4', 'type' => 'longText']
-        ];
- 
-        return $this->createTable($table_name, $fields);
     }
 
-    public function createTable($table_name, $fields = [])
-    {
-        // check if table is not already exists
-        if (!Schema::hasTable($table_name)) {
-            Schema::create($table_name, function (Blueprint $table) use ($fields, $table_name) {
-                $table->increments('id');
-                if (count($fields) > 0) {
-                    foreach ($fields as $field) {
-                        $table->{$field['type']}($field['name']);
-                    }
-                }
-                $table->timestamps();
-            });
- 
-            return response()->json(['message' => 'Given table has been successfully created!'], 200);
-        }
- 
-        return response()->json(['message' => 'Given table is already existis.'], 400);
-    }
+    
 
 
 }
