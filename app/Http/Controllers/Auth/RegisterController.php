3618b18d5226c4289fd56e0a87c3ad8df7463419
dammydev2 <yd4u2c@yahoo.com>
2020-03-14 15:10:56 +0100

    input update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        //here is where the promo enteres
+        //here is where the promo entereskkk
         Promoter::create([
             'name' => $data['name'],
             'contact_name' => $data['contact_name'],
